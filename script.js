17a8c180933f01e22305c4eee582fe3b349528eb
MaatheusOM1 <matheus.macedo2005@gmail.com>
2023-09-26 15:27:35 -0300

    Commit 6

--- script.js
+++ script.js
@@ -21,11 +21,12 @@ const sova = document.querySelector('.sova')
 const viper = document.querySelector('.viper')
 const yoru = document.querySelector('.yoru')
 
-
-
 const tempoNaTela = document.querySelector('#timer')
 let tempoDecorridoEmSegundos = 90
 let intervaloId = null
+
+const infoAgentes = document.querySelector('.container__wrap')
+const agentePickado = document.querySelector('#agente__pickado')
 
 
 //Função para a contagem regressiva do contador
@@ -55,3 +56,42 @@ function pararContagem(){
 
 mostrarTempo() //Puxa a função no escopo global para sempre aparecer o cronômetro na tela
 iniciarOuPausar() //Puxa a função no escopo global para já começar o contador
+
+
+
+
+
+astra.addEventListener('click', () =>{
+    infoAgentes.innerHTML = `
+    <div class="container__informacoes-agentes">
+    <h3 class="role__agente">Controlador</h3>
+    <h1 class="nome__agente">ASTRA</h1>
+    <div class="container__skills">
+        <div class="role__agente-icone skills">
+            <p>INFO</p>
+            <img src="/images/controlador.png" alt="" class="skills__logo">
+        </div>
+        <div class="skill-c skills">
+            <p>C</p>
+            <img src="/images/astra c.png" alt="" class="skills__logo">
+        </div>
+        <div class="skill-q skills">
+            <p>Q</p>
+            <img src="/images/astra q.png" alt="" class="skills__logo">
+        </div>
+        <div class="skill-e skills">
+            <p>E</p>
+            <img src="/images/astra e.png" alt="" class="skills__logo">
+        </div>
+        <div class="skill-x skills">
+            <p>X</p>
+            <img src="/images/astra x.png" alt="" class="skills__logo">
+        </div>
+    </div>
+    <p class="info__agente">Astra, a Agente ganense, utiliza energias cósmicas para moldar o campo de batalha a seu bel-prazer. Com total domínio da sua forma astral e um talento estratégico nato, ela está sempre anos-luz à frente dos inimigos.</p>
+    <b class="role__agente">Controlador</b>
+    <p class="info__role">Controladores se dedicam a dissecar territórios perigosos e pavimentar o caminho do sucesso para a equipe.</p>
+</div>
+    `
+    agentePickado.setAttribute('src', `./images/astra.png`)//Trocar a foto de acordo com a página
+})
